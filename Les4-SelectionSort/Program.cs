96a2ea4a106a28d2da55359d1e83cda55845e026
Айкин Иван <ivan.aykin@mail.ru>
2023-01-12 19:45:00 +0700

    Добавлен простой пример сортировки выбором

--- Les4-SelectionSort/Program.cs
+++ Les4-SelectionSort/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сортировка выбором от min к max
+
+void SortSelection(int[] collection)
+{
+    int size = collection.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i]; // временная переменная
+        collection[i] = collection[pos]; // текущее значение, которое мы заменяем на меньшее
+        collection[pos] = temp; // значение, которое ставим на место предыдущего минимального
+    }
+}
+
+int[] array = { 7, 6, 3, 4, 5, 1, 2, 3 };
+System.Console.WriteLine($"[{String.Join(", ",array)}]"); // Выводим массив на экран
+
+SortSelection(array);
+System.Console.WriteLine($"[{String.Join(", ",array)}]");
